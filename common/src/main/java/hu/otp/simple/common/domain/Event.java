8d399af0e695ca677e2481679afad70212f295be
vforjan <valentin.forjan@stratis.hu>
2021-07-21 00:31:17 +0200

    Refactor.

---
+++ common/src/main/java/hu/otp/simple/common/domain/Event.java
@@ -0,0 +1,53 @@
+package hu.otp.simple.common.domain;
+
+public class Event {
+
+	private int eventId;
+	private String title;
+	private String location;
+	private String startTimeStamp;
+	private String endTimeStamp;
+
+	public int getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(int eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getStartTimeStamp() {
+		return startTimeStamp;
+	}
+
+	public void setStartTimeStamp(String startTimeStamp) {
+		this.startTimeStamp = startTimeStamp;
+	}
+
+	public String getEndTimeStamp() {
+		return endTimeStamp;
+	}
+
+	public void setEndTimeStamp(String endTimeStamp) {
+		this.endTimeStamp = endTimeStamp;
+	}
+
+	// TODO: handy
+	// String date = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (epoch*1000))
+}
---
+++ common/src/main/java/hu/otp/simple/common/domain/EventWrapper.java
@@ -0,0 +1,26 @@
+package hu.otp.simple.common.domain;
+
+import java.util.List;
+
+public class EventWrapper {
+
+	private List<Event> data;
+	private boolean success;
+
+	public List<Event> getData() {
+		return data;
+	}
+
+	public void setData(List<Event> data) {
+		this.data = data;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+}
